e9ff34cc2e90aade770e85ee042f7aa41ae1aad1
dany <dany@dany-Nitro-AN515-51>
2019-02-27 22:17:01 -0600

    3.1

---
+++ ExcelColumns/src/excelcolumns/ExcelColumns.java
@@ -0,0 +1,68 @@
+package excelcolumns;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dany
+ */
+public class ExcelColumns {
+
+    
+    public static void main(String[] args) {
+        String[] columnNames = generateExcelColumnNames(350);
+        
+        for (String columnName : columnNames)
+        {
+            System.out.print(columnName + " ");
+        }
+    }
+    
+    public static String[] generateExcelColumnNames(int columnCount)
+    {
+        String[] columns = new String[columnCount];
+        int caracter = 65;
+        int posicion=0;
+        columns[0]="";
+        int indicador=0;
+        if(columnCount<1)
+        {
+            columns[0]="Cantidad debe de ser mayor a 1";
+            return columns;
+        }        
+        if(columnCount>10000)
+        {
+            columns[0]="Cantidad debe de ser menor a 10000";
+            return columns;
+        }
+        for (int i=0;i<columnCount;i++)
+        {
+            if(indicador==0)
+            {
+                columns[i]=""+Character.toString((char)caracter);
+            }
+            if(indicador!=0)
+            {
+                columns[i]=columns[posicion]+Character.toString((char)caracter);
+            }
+            if(caracter == 90)
+            {
+                caracter = 64;
+                if(indicador==0)
+                {
+                    indicador++;
+                }
+                else
+                {
+                    posicion = posicion+1;
+                }
+            }
+            caracter++;
+            
+        }
+        
+        
+        return columns;
+    }
+    
+}
